9993f22f3254d3041e43377fbb2c4264f28a97af
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 18:46:36 +0500

    task

--- bin/mana.dart
+++ bin/mana.dart
@@ -1,13 +1,23 @@
 void main() {
-  String massege = 'Salom';
-  String talk = 'salim';
-  print(text(talk));
+  String q = 'qaleysan';
+  String w = 'qayerdasan';
+  String r = "qachon kelasan";
+  String t = 'Kechga';
+  print(text(w));
 }
 
 String text(String text) {
   String ans = '';
+
   if (text == 'Salom' || text == "salom" || text == "salim") {
     ans = 'alik baraka top bolam!';
+  } else if (text == "qaleysan") {
+    return 'yaxwi, rahmat';
+  } else if (text == 'qayerdasan') {
+    return 'nima ishing bor';
+  } else if (text == 'qachon kelasan') {
+    return 'Kechga';
   }
+
   return ans;
 }
